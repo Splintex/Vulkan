be20049e8eb50519da34136cdc42ba3823ee2c00
Yevgen <godun.yevgen@gmail.com>
2013-03-21 10:00:17 +0200

    upd

--- js/common.js
+++ js/common.js
@@ -1,20 +1,8 @@
 $(document).ready(function() {
 
-    $(".js-key").click(function(){
-        if ($(this).hasClass("js-active")) {
-            $(this).removeClass("js-active");
-            $(this).children().animate({
-                left: -1,
-            }, 200);
-            $(this).children().text("off");
-        }
-        else {
-            $(this).addClass("js-active");
-            $(this).children().animate({
-                left: 27,
-            }, 200);
-            $(this).children().text("on");
-        }
+    $(".key button").click(function(){
+        $(".js-keys button").removeClass("is-active");
+        $(this).addClass("is-active");
     });
 
 });
